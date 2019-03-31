3af30f40770aa270bf2300eb0bad27db42383e18
devpang <devpang20@gmail.com>
2019-03-31 16:19:12 +0900

    todostudy

---
+++ my-react/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import MyComponent from './MyComponent';
+
+
+class App extends Component {
+  render() {
+    return (
+        <MyComponent />
+    );
+  }
+}
+
+export default App;
