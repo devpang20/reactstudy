71a46b2e04f9f1967a3962a33b196d6cd834a221
devpang <devpang20@gmail.com>
2019-03-31 16:44:39 +0900

    step2

---
+++ my-react/src/MyComponent.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class MyComponent extends Component {
+    render() {
+        return (
+            <div>
+                첫 번째 컴포넌트
+            </div>
+        );
+    }
+}
+
+export default MyComponent;
