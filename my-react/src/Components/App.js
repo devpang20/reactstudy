59aa8da46cba246ce4ed644518d1b6ad3dfc0650
pangsik <devpang20@gamil.com>
2019-04-05 11:06:18 +0900

    push

--- my-react/src/Components/App.js
+++ my-react/src/Components/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
-import MyComponent from './MyComponent';
 
 class App extends Component {
   render() {
     return (
-      <MyComponent name = "김또깡"/>
+      <>
+        Todo App
+      </>
         );
   }
 }
